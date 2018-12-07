dfbf1d6eb5fd88e21c44ab6f719f6aa937f2f191
Robert Barnett <robert.barnett@laptop210.ma.us>
2018-12-07 13:40:52 -0500

    make upload form a template and require pdfs only.

--- s3.py
+++ s3.py
@@ -7,9 +7,11 @@ aws_secret_access_key=ACCESS_KEY
 pip install boto3
 '''
 
-from flask import Flask, request, render_template
+from flask import Flask, flash, request, render_template, redirect
 from livereload import Server, shell
 import boto3
+
+ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
 
@@ -17,32 +19,47 @@ app = Flask(__name__)
 # https://github.com/lepture/python-livereload/issues/144
 app.debug = True
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def index():
-    return '''<form method=POST enctype=multipart/form-data action="upload">
-    <input type=file name=s3file>
-    <input type=submit>
-    </form>'''
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    s3 = boto3.client(
-        "s3",
-        aws_access_key_id='[ACCESS_KEY]',
-        aws_secret_access_key='[SECRET_KEY]'
-    )
+    if 's3file' not in request.files:
+        flash('Something went wrong. No file found')
+        return redirect(request.url)
 
-    s3.upload_fileobj(
-        request.files['s3file'],
-        'edb-cdn-downloads',
-        'docs/' + request.files['s3file'].filename,
-        ExtraArgs={
-            "ACL": "public-read",
-            "ContentType": request.files['s3file'].content_type
-        }
-    )
+    if request.files['s3file'].filename == '':
+        flash('No file selected')
 
-    return '<h3>File saved to s3</h3>'
+    if request.files['s3file'] and allowed_file(request.files['s3file'].filename):
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id='[ACCESS_KEY]',
+            aws_secret_access_key='[SECRET_KEY]'
+        )
+
+        s3.upload_fileobj(
+            request.files['s3file'],
+            'edb-cdn-downloads',
+            'docs/' + request.files['s3file'].filename,
+            ExtraArgs={
+                "ACL": "public-read",
+                "ContentType": request.files['s3file'].content_type
+            }
+        )
+
+        file_url = 'https://get.enterprisedb.com/docs/' + request.files['s3file'].filename
+        return '''<h3>File saved to s3 bucket</h3>
+                <p><a href="''' + file_url + '" target="_blank">' + file_url + '''</a></p>
+                <p><a href="/">Upload another file</a></p>'''
+    else:
+        return '''<h5>You can only upload a PDF.</h5>
+                <p><a href="/">Upload another file</a></p>'''
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
