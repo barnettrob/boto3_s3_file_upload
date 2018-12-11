356b5346f4b640bb6e3604b0ee489cca1b733dd9
Robert Barnett <robert.barnett@laptop210.ma.us>
2018-12-11 16:29:03 -0500

    modifications to work with zappa and lambda

--- s3.py
+++ s3.py
@@ -1,23 +1,10 @@
-'''
-You need to have AWS credentials in ~/.aws/credentials
-[default]
-aws_access_key_id=KEY_ID
-aws_secret_access_key=ACCESS_KEY
-
-pip install boto3
-'''
-
 from flask import Flask, flash, request, render_template, redirect
-from livereload import Server, shell
 import boto3
+import os
 
 ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
-
-# remember to use DEBUG mode for templates auto reload
-# https://github.com/lepture/python-livereload/issues/144
-app.debug = True
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -25,10 +12,12 @@ def allowed_file(filename):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', path=request.script_root)
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    index_page = request.url_root
+
     if 's3file' not in request.files:
         flash('Something went wrong. No file found')
         return redirect(request.url)
@@ -37,14 +26,23 @@ def upload():
         flash('No file selected')
 
     if request.files['s3file'] and allowed_file(request.files['s3file'].filename):
-	# Uncomment below client call with aws_access_key_id and aws_secret_access_key if no credentials file in .aws folder. --FOR TESTING ONLY. Insecure to include in code.
-	# Don't forget to comment out the client call with only "s3".
-        # s3 = boto3.client(
-        #     "s3",
-        #     aws_access_key_id='[ACCESS_KEY]',
-        #     aws_secret_access_key='[SECRET_KEY]'
-        # )
-        s3 = boto3.client("s3")
+        # Store access key and secret key for s3 bucket uploading to in a config file
+        # uploaded to your other s3 bucket. Point to this config file in zappa_settings.json:
+        # {
+        #     "dev": {
+        #         ...
+        #     "remote_env": "s3://my-other-bucket/super-secret-config.json",
+        # },
+        # ...
+        # }
+        access_key = os.environ.get('access_key')
+        secret_key = os.environ.get('secret_key')
+
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key
+        )
 
         s3.upload_fileobj(
             request.files['s3file'],
@@ -59,17 +57,11 @@ def upload():
         file_url = 'https://get.enterprisedb.com/docs/' + request.files['s3file'].filename
         return '''<h3>File saved to s3 bucket</h3>
                 <p><a href="''' + file_url + '" target="_blank">' + file_url + '''</a></p>
-                <p><a href="/">Upload another file</a></p>'''
+                <p><a href="''' + index_page + '">Upload another file</a></p>'
     else:
         return '''<h5>You can only upload a PDF.</h5>
-                <p><a href="/">Upload another file</a></p>'''
+                <p><a href="''' + index_page + '">Upload another file</a></p>'
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
 
-# Live reload server
-# Prob want to set some sort of env flag to only do this on DEV
-server = Server(app.wsgi_app)
-
-# use custom host and port
-server.serve(port=80, host='0.0.0.0')
